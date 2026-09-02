6804ebbd5be44232c32ac4273eb3eab70e64b030
Morimel <melsovisa77@gmail.com>
2026-09-02 17:00:13 +0800

    Initial Commit

---
+++ ColorPicker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ColorPicker
+//
+//  Created by Isa Melsov on 2/9/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
